4cda8f1ac75035f7e409131788e1f845bfeb6a1d
monty <monty@taolam.com>
2011-04-13 13:44:48 -0600

    Initial revision

---
+++ sh-archive/ap-manual-mod.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+AtomicParsley "$1" --manualAtomRemove "moov.udta.meta.ilst.apID" --manualAtomRemove "moov.udta.meta.ilst.cnID" --manualAtomRemove "moov.udta.meta.ilst.atID" --manualAtomRemove "moov.udta.meta.ilst.cmID" --manualAtomRemove "moov.udta.meta.ilst.plID" --manualAtomRemove "moov.udta.meta.ilst.sfID" --manualAtomRemove "moov.udta.meta.ilst.purd"
