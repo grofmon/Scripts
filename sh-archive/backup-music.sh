ef50905564c33cd4d6ed6e04e51b5e97419c5464
monty <monty@taolam.com>
2012-05-31 11:56:27 -0600

    Changed rsync command

--- sh-archive/backup-music.sh
+++ sh-archive/backup-music.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-rsync -amv --delete "/Users/monty/Music/" "/Volumes/2TB WD/Music/"
+DATE=`date "+ %Y-%m-%d"`
+#rsync -amv --numeric-ids --delete-during --backup --backup-dir=_mng_archive/$DATE "/Users/monty/Music/" "/Volumes/2TB WD/Music/"
+rsync -amv --numeric-ids --delete-during --backup --backup-dir=_mng_archive/$DATE "/Volumes/Parker-Spare/music-not-in-itunes/" "/Volumes/2TB WD/music-backup/music-not-in-itunes/"
