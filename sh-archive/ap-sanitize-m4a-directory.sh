2b43c436bcd576e70b7c2875af5f627590e43e0c
monty <monty@taolam.com>
2012-06-02 15:01:29 -0600

    Updated iTunes media directory path

--- sh-archive/ap-sanitize-m4a-directory.sh
+++ sh-archive/ap-sanitize-m4a-directory.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 # Remove encoding atoms and move to iTunes 
-find . -iname "*m4a" -execdir ~/Library/Scripts/ap {} --encodingTool "" --encodedBy "" --output ~/Music/iTunes/iTunes\ Media/Automatically\ Add\ to\ iTunes/{} \; -execdir mv {} ~/.Trash \;
+find . -iname "*m4a" -execdir ~/Library/Scripts/ap {} --encodingTool "" --encodedBy "" --output ~/Music/iTunes/Media/Automatically\ Add\ to\ iTunes/{} \; -execdir mv {} ~/.Trash \;
 
