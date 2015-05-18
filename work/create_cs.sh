b1d69e1a5e499d797b530ffef8de33fb482c68ba
Montgomery Groff <montgomery.groff@echostar.com>
2015-05-18 12:11:33 -0600

    modified to work with XIP or ZIP models

--- work/create_cs.sh
+++ work/create_cs.sh
@@ -6,14 +6,18 @@ label=$3
 if [ -z "$config_spec" -o -z "$model" -o -z "$label" ]; then
     echo "usage: create_cs.sh <config_spec> <model> <label>"
     echo "       where: "
-    echo "             config_spec = input config spec with approprirate model tags"
-    echo "             model = model to generate output config spec"
-    echo "             label = label name for config spec"
+    echo "             config_spec = input config spec with appropriate model tags"
+    echo "             model       = model to generate output config spec"
+    echo "             label       = label name for config spec"
     echo ""
-    echo "       e.g.  create_cs.sh ~/XIPCOMBINED.cs 913 BRC1"
-    echo "     output  XIP913_BRC1.cs"
+    echo "       e.g.  create_cs.sh ~/XIPCOMBINED.cs XIP913 BRC1"
+    echo ""
+    echo "       output ->  XIP913_BRC1.cs"
     exit
 fi
 
-sed 's/\#>XIP'$2'\#> //' ${config_spec} > XIP${model}_${label}.cs
-sed -i '/#>/d' XIP${model}_${label}.cs
+sed 's/\#>'$2'\#> //' ${config_spec} > ${model}_${label}.cs
+sed -i '/#>/d' ${model}_${label}.cs
+sed -i '/## XIPDEV/d' ${model}_${label}.cs
+sed -i '/## RIGEL/d' ${model}_${label}.cs
+sed -i '/## Sanitize/d' ${model}_${label}.cs
